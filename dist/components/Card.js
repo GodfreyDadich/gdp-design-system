60c4ebe101d76e05a97ad04af95f234ba9b8d9f3
Blaine OBrien <blaineo@gmail.com>
2018-11-20 11:14:34 -0800

    chore( fixing issue with container classes being passed to image in the Card element )

--- dist/components/Card.js
+++ dist/components/Card.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _style = require('styled-jsx/style');
 
 var _style2 = _interopRequireDefault(_style);
@@ -26,23 +28,29 @@ var _CardText2 = _interopRequireDefault(_CardText);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var imagePropsObject = function imagePropsObject(props) {
+  var newObj = _extends({}, props);
+  delete newObj.classAdd;
+  return newObj;
+};
+
 var Card = function Card(props) {
   return _react2.default.createElement(
     'div',
     {
-      className: _style2.default.dynamic([['184887790', [props.mediaOrientation === 'left' || props.mediaOrientation === 'right' ? '50%' : '100%']]]) + ' ' + (props.classAdd || '')
+      className: _style2.default.dynamic([['1927751783', [props.mediaOrientation === 'left' || props.mediaOrientation === 'right' ? '50%' : '100%']]]) + ' ' + (props.classAdd || '')
     },
     !!(props.mediaOrientation === 'bottom' || props.mediaOrientation === 'right') && _react2.default.createElement(_CardText2.default, props),
     _react2.default.createElement(
       'div',
       {
-        className: _style2.default.dynamic([['184887790', [props.mediaOrientation === 'left' || props.mediaOrientation === 'right' ? '50%' : '100%']]]) + ' ' + 'cardMedia'
+        className: _style2.default.dynamic([['1927751783', [props.mediaOrientation === 'left' || props.mediaOrientation === 'right' ? '50%' : '100%']]]) + ' ' + 'cardMedia'
       },
-      _react2.default.createElement(_Image2.default, props)
+      _react2.default.createElement(_Image2.default, imagePropsObject(props))
     ),
     !!(props.mediaOrientation === 'top' || props.mediaOrientation === 'left') && _react2.default.createElement(_CardText2.default, props),
     _react2.default.createElement(_style2.default, {
-      styleId: '184887790',
+      styleId: '1927751783',
       css: '.cardMedia.__jsx-style-dynamic-selector{display:inline-block;width:' + (props.mediaOrientation === 'left' || props.mediaOrientation === 'right' ? '50%' : '100%') + ';}',
       dynamic: [props.mediaOrientation === 'left' || props.mediaOrientation === 'right' ? '50%' : '100%']
     })
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,19 +3,25 @@ import ConditionalLink from './ConditionalLink'
 import Image from './Image'
 import CardText from './CardText'
 
-const Card = props => 
-<div className={props.classAdd} >
-  {!!(props.mediaOrientation === 'bottom' || props.mediaOrientation === 'right') && <CardText {...props} />}
-  <div className='cardMedia'>
-    <Image {...props} />
+const imagePropsObject = props => {
+  const newObj = Object.assign({}, props)
+  delete newObj.classAdd
+  return newObj
+}
+
+const Card = props =>
+  <div className={props.classAdd} >
+    {!!(props.mediaOrientation === 'bottom' || props.mediaOrientation === 'right') && <CardText {...props} />}
+    <div className='cardMedia'>
+      <Image {...imagePropsObject(props)} />
+    </div>
+    {!!(props.mediaOrientation === 'top' || props.mediaOrientation === 'left') && <CardText {...props} />}
+    <style jsx>{`
+      .cardMedia {
+        display: inline-block;
+        width: ${props.mediaOrientation === 'left' || props.mediaOrientation === 'right' ? '50%' : '100%'};
+      }
+    `}</style>
   </div>
-  {!!(props.mediaOrientation === 'top' || props.mediaOrientation === 'left') && <CardText {...props} />}
-  <style jsx>{`
-    .cardMedia {
-      display: inline-block;
-      width: ${props.mediaOrientation === 'left' || props.mediaOrientation === 'right' ? '50%' : '100%'};
-    }
-  `}</style>
-</div>
 
 export default Card
