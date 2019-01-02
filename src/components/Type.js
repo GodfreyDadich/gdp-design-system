9ee285b23d29e4ac2ac3d2d67367363acd540676
Blaine OBrien <blaineo@gmail.com>
2019-01-02 15:48:18 -0800

    chore( adding a font size for pullquotes on tablet )

--- src/components/Type.js
+++ src/components/Type.js
@@ -163,6 +163,10 @@ export const Pullquote = ({ classAdd, children }) =>
             letter-spacing: -0.3px;
             text-align: center;
             margin-bottom: 89px;
+
+            @media only screen and (max-width: 768px) {
+              font-size: 3.34vw;
+            }             
           }
         `}</style>
       </div>
