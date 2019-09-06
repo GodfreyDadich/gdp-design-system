3230fc6450257ee75ee93da8d9c85f947b71b896
Blaine OBrien <blaineo@gmail.com>
2019-09-06 10:13:47 -0700

    chore( fix for mobile dynamic loading )

--- src/components/Museum.js
+++ src/components/Museum.js
@@ -70,6 +70,7 @@ const Museum = ({ galleries, columns, thumbAspect, countIndicator, containerAspe
                 aspectRatio={thumbAspect || 'sixteen'}
                 imgSource={gallery.thumb.length > 0 ? gallery.thumb : gallery.images[0]}
                 skipIntro
+                visibilityOverride
               />
             </div>
           )
