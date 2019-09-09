63dc0766e374a7143ed644581cfbacf04a593e0d
Melika Kalbasi <melikalbasi@gmail.com>
2019-09-09 11:26:17 -0700

    chore( create closeModal button through css instead of svg )

--- src/stories/Icons.js
+++ src/stories/Icons.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { InstagramIcon, LinkedInIcon } from '../components/Icons'
+import { InstagramIcon, LinkedInIcon, CloseModal } from '../components/Icons'
 
 const Icons = props =>
   <div>
     Instagram:&nbsp;
     <InstagramIcon color='green' hoverColor='red' /><br /> <br />
     LinkedIn:&nbsp;
-    <LinkedInIcon color='green' />
+    <LinkedInIcon color='green' /><br /> <br />
+    Close Modal:&nbsp;
+    <CloseModal color='green' />
   </div>
 
 export default Icons
