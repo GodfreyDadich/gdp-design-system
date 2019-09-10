89a6e322c40bf8ab1907fe4e45841da7c456bc14
Melika Kalbasi <melikalbasi@gmail.com>
2019-09-10 15:53:26 -0700

    chore( reorganize Colors story )

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,55 +1,53 @@
-import { rgba } from "../utils/color";
-
-// Background colors
-const backgroundHome = '#000'
-const backgroundPrimary = '#fff'
-
-// Link colors
-const linkPrimary = '#333'
-const gdpNeon = '#00FF00'
-
 // Text colors
-const textPrimary = '#333'
-const textSecondary = '#6D6A60'
+const textGray = '#7F7F7F'
+const textMatteBlack = '#333'
 
-// Accent colors
-const gdpOrange = '#FF6600'
+// UI Elements
+const blackUI = '#000'
+const gdpNeonUI = '#00FF00'
+const gdpGrayUI = '#6D6A60'
+const whiteUI = '#fff'
+
+// GDP Primary Colors
+const gdpPrimaryOrange = '#FF6600'
+const gdpPrimaryGreen = '#00AD68'
+const gdpPrimaryTeal = '#0C9094'
+
+// Gilmore Palette colors
 const gdpOrange1 = '#EC6724'
 const gdpOrange2 = '#EE8028'
-const gdpGreen = '#00AD68'
-const gdpTeal = '#0C9094'
-const gdpRed = '#A9233A'
-const gdpRed1 = '#D72637'
-const gdpRed2 = '#EE2E2B'
-const gdpRed3 = '#EA4F29'
-const gdpGold = '#E99E28'
-const gdpGold2 = '#E4BA3C'
-const gdpBlue = '#008B9B'
-const gdpBlue1 = '#00B1BF'
-const gdpBlue2 = '#5EBBBC'
+const gilmoreRed = '#A9233A'
+const gilmoreRed1 = '#D72637'
+const gilmoreRed2 = '#EE2E2B'
+const gilmoreRed3 = '#EA4F29'
+const gilmoreGold = '#E99E28'
+const gilmoreGold2 = '#E4BA3C'
+const gilmoreBlue = '#008B9B'
+const gilmoreBlue1 = '#00B1BF'
+const gilmoreBlue2 = '#5EBBBC'
 
 export default {
-  backgroundHome,
-  backgroundPrimary,
+  textGray,
+  textMatteBlack,
+  
+  blackUI,
+  gdpNeonUI,
+  gdpGrayUI,
+  whiteUI,
 
-  linkPrimary,
-  gdpNeon,
+  gdpPrimaryOrange,
+  gdpPrimaryGreen,
+  gdpPrimaryTeal,
 
-  textPrimary,
-  textSecondary,
-
-  gdpOrange,
   gdpOrange1,
   gdpOrange2,
-  gdpGreen,
-  gdpTeal,
-  gdpBlue,
-  gdpBlue1,
-  gdpBlue2,  
-  gdpRed,
-  gdpRed1,
-  gdpRed2,
-  gdpRed3,
-  gdpGold,
-  gdpGold2
+  gilmoreRed,
+  gilmoreRed1,
+  gilmoreRed2,
+  gilmoreRed3,
+  gilmoreGold,
+  gilmoreGold2,
+  gilmoreBlue2,  
+  gilmoreBlue1,
+  gilmoreBlue,
 };
