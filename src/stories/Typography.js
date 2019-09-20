7b71d74038ab915ec45b41c5c0f41a40cdff5157
Melika Kalbasi <melikalbasi@gmail.com>
2019-09-20 09:50:07 -0700

    chore( remove unused types )

--- src/stories/Typography.js
+++ src/stories/Typography.js
@@ -87,29 +87,6 @@ const Typography = props =>
     </div>
 
     <br /><br />
-    
-
-    {/* <div>
-      <div style={styles.smallText}><small>Super ({fontSizeSuper}pt)</small></div>
-      <HeadingSuper>Top experiences in Asia</HeadingSuper>
-    </div> */}
-
-    {/* <br /><br /> */}
-
-
-    {/* <div>
-      <div style={styles.smallText}><small>Heading 2 ({fontSizeHeading2}pt)</small></div>
-      <Heading2>Top experiences in Asia</Heading2>
-    </div>
-
-    <br /><br />
-
-    <div>
-      <div style={styles.smallText}><small>Heading 3 ({fontSizeHeading3}pt)</small></div>
-      <Heading3>Top experiences in Asia</Heading3>
-    </div>
-
-    <br /><br />
 
     <div>
       <div style={styles.smallText}><small>Heading 4 ({fontSizeHeading4}pt)</small></div>
@@ -131,10 +108,10 @@ const Typography = props =>
       <SubHeadBlack>Top experiences in Asia</SubHeadBlack>
     </div>
 
-    <br /><br /> */}
+    <br /><br />
 
 
-    {/* <div>
+     <div>
       <div style={styles.smallText}><small>Bio Header Lockup</small></div>
       <BioHeader
         personName='Scott Dadich'
@@ -143,53 +120,15 @@ const Typography = props =>
       />
     </div>
 
-    <br /><br /> */}
-
-    {/* <div>
-      <div style={styles.smallText}><small>Heading 5 ({fontSizeHeading5}pt)</small></div>
-      <div style={textHeading5()}>Top experiences in Asia</div>
-    </div>
-
-    <br /><br />
-
-    <div>
-      <div style={styles.smallText}><small>Heading 6 ({fontSizeHeading6}pt)</small></div>
-      <div style={textHeading6()}>Top experiences in Asia</div>
-    </div>
-
-    <br /><br />
-
-    <div>
-      <div style={styles.smallText}><small>Heading 7 ({fontSizeHeading7}pt)</small></div>
-      <div style={textHeading7()}>Top experiences in Asia</div>
-    </div>
-
-    <br /><br />
-
-    <div>
-      <div style={styles.smallText}><small>Heading 8 ({fontSizeHeading8}pt)</small></div>
-      <div style={textHeading8()}>Top experiences in Asia</div>
-    </div>
-
-    <br /><br /> */}
-
-    {/* <div>
-      <div style={styles.smallText}><small>Body article ({fontSizeBodyArticle}pt)</small></div>
-      <p>
-        Few magazines in the world are as revered as <span style={{ fontStyle: 'italic' }}>National Geographic</span>. For 130 years its breathtaking photography and groundbreaking narratives have helped readers discover the world through its pages. While the brand has evolved with a changing industry—building a thriving television network, books and kids divisions, travel business, and social media audience (90+ million Instagram followers and counting)—the magazine itself hadn’t undergone a significant redesign for two decades.
-        </p>
-    </div>
-
-    <br /><br /> */}
-
+    <br /><br /> 
 
     <div>
       <div style={styles.smallText}><small>Atlas Grotesk PQ</small></div>
       <Pullquote classAdd='testadd'>
       The editorial team created new story forms and structures that could tackle big, bold ideas with provocative voice.
-    {/* <QuoteAttribution classAdd='testadd'>
+    <QuoteAttribution classAdd='testadd'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    </QuoteAttribution> */}
+    </QuoteAttribution>
       </Pullquote>
     </div>
 
@@ -205,7 +144,7 @@ const Typography = props =>
 
     <br /><br />
 
-    {/* <div>
+    <div>
       <div style={styles.smallText}><small>Small Caps (Noe Text) ({fontSizeHeading4}pt)</small></div>
       <WiredType><span style={{ fontFamily: 'Noe Text' }}>wired</span></WiredType>
     </div>
@@ -217,80 +156,14 @@ const Typography = props =>
       <InquiryText><h5>For speaking engagement inquiries, please contact inquiries@godfreydadich.com</h5></InquiryText>
     </div>
 
-    <br /><br />
-
-    <div>
-      <div style={styles.smallText}><small>Body small ({fontSizeBodySmall}pt)</small></div>
-      <div style={textBodySmall()}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed
-        mauris sit amet massa interdum bibendum. Ut ac ex leo. Cras blandit
-        enim ut metus feugiat, vitae pharetra massa aliquet.
-        </div>
-    </div>
-
-    <br /><br />
+    <br />
 
     <div>
       <div style={styles.smallText}><small>Accent ({fontSizeAccent}pt)</small></div>
       <div style={textAccent()}>
         Lorem ipsum dolor sit amet
         </div>
-    </div> */}
-
-    {/* <br /><br />
-
-    <div>
-      <div style={styles.smallText}><small>Left Carving</small></div>
-      <div style={{ width: '65%', margin: 'auto', fontSize: '15px', fontFamily: 'Noe Text' }}>
-        <LeftCarving>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        </LeftCarving>
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-      </div>
     </div>
-
-    <br /><br />
-
-    <div>
-      <div style={styles.smallText}><small>Quote Block</small></div>
-      <div>
-        <Pullquote pqOptions='quote-block'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        </Pullquote>
-      </div>
-    </div>
-
-    <br /><br />
-
-    <div>
-      <div style={styles.smallText}><small>Sidebar Quote</small></div>
-      <div>
-        <SidebarQuote>
-          Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        </SidebarQuote>
-      </div>
-      <div style={{ width: '59%', fontSize: '15px', margin: 'auto', fontFamily: 'Noe Text' }}>
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        Lorem ipsum dolor sit amet, adipiscing elit. Curabi in lobortis massa, at mollis mauris. nec commodo eros, id hendrerit est. Vestibulum eu volutpat  Nam id mauris metus.
-        </div>
-    </div> */}
 
   </div>
 
