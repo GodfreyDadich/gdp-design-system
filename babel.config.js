77b8a5343b4ec1794f3d46598eb0d875f8f81d69
Blaine OBrien <blaineo@gmail.com>
2019-10-04 11:08:59 -0700

    chore( got story source working!!! )

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = api => {
+  api.cache(true);
+
+  return {
+    presets: ['@babel/env', '@babel/react'],
+    plugins: [
+      '@babel/plugin-proposal-object-rest-spread', 
+      '@babel/transform-runtime',
+      ['styled-jsx/babel', { 'plugins': ['styled-jsx-plugin-sass'] }]],
+    env: {
+      test: {
+        plugins: ['require-context-hook']
+      }
+    }
+  }
+}
